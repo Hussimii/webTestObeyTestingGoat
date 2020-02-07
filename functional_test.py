123280da330e34986c76e857c82ea399cacc5401
xwr <riscn_xz@hotmail.com>
2020-02-07 17:57:01 +0800

    Extend Functional_test.py with unittest.

--- functional_test.py
+++ functional_test.py
@@ -1,6 +1,22 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get("http://localhost:8000")
 
-assert 'Django' in browser.title
+class NewVisitorTest(unittest.TestCase):
+
+    def setUp(self):
+        self.newBrowser = webdriver.Firefox()
+
+    def tearDown(self):
+        self.newBrowser.quit()
+
+    def test_canStartAListAndRetrieveItLater(self):
+        self.newBrowser.get("http://localhost:8000")
+        self.assertIn("To-Do", self.newBrowser.title)
+        self.fail("Finish the test.")
+
+    # ...
+
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
